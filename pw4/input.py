9f54173961abd72edc74cddb1c5dda4903ff0555
ibiann <trungpham152000@gmail.com>
2021-04-26 16:17:24 +0700

    fix pw4

--- pw4/input.py
+++ pw4/input.py
@@ -1,49 +1,25 @@
-# packages
-import math
 import numpy
-import curses
-from domains.Student import *
-from domains.Course import *
-from domains.Mark import *
+import math
 
+def AddInforStudent():
+    id = input("Enter Id: ")
+    name = input("Enter Name: ")
+    dob = input("Enter dob: ")
+    student = Student(id, name, dob)
+    students.append(student)
 
-class InputModule:
-    # Input total number of students
-    @staticmethod
+def AddInforCourse():
+    id = input("Enter Id: ")
+    name = input("Enter name: ")
+    credit = input("Enter credit: ")
+    course = Course(id, name, credit)
 
-    def student_num():
-        print("**Total Students**")
-        student = int(input("Enter total number of student: "))
-        return student
-
-# Add student
-
-    print("**Add student**")
-    id = input("Id: ")
-    Name = input("Name: ")
-    dob = input("DOB: ")
-    student = Student(id, Name, dob)
-    Student.append(student)
-    Student.append(self.id)
-
-# Add course
-    @staticmethod
-    def add_Course():
-        print("**Add course**")
-        id = input("id: ")
-        name = input("name: ")
-        credit = input("credit: ")
-        course = Course(id, name, credit)
-        Courses.append(course)
-
-# Add mark
-    @staticmethod
-def Add_Mark():
-    courseName = input("Enter the Mark of the courses: ")
-    for C in Courses:
-        if C.getName() == courseName:
-            for S in Student:
-                Mark = math.floor(float(input("Enter " + S.getName() + "'S Mark: ")))
-                StudentMark = Marks(S, C, Mark)
-                StudentMarks.append(StudentMark)
-                S.setAvg(calculateAvg(S.getId()))
+def inputMark():
+    courseName = input("Input course's name to input marks: ")
+    for c in courses:
+        if c.getName() == courseName:
+            for s in students:
+                mark = math.floor(float(input("Input " + s.getName() + "'s mark: ")))
+                studentMark = StudentMark(s, c, mark)
+                studentMarks.append(studentMark)
+                s.setAvg(calculateAvg(s.getId()))
--- pw4/output.py
+++ pw4/output.py
@@ -1,33 +1,19 @@
-import numpy
-import curses
-from domains.Student import *
-from domains.Course import *
-from domains.Mark import *
+# Display
+def ShowlistCourses():
+    print("Show course List:")
+    for c in courses:
+        c.describe()
 
-class OutputModule:
-    @staticmethod
-    def ShowlistCourses():
-        print("Show course list:")
-        for c in Courses:
-            c.describe()
 
-    @staticmethod
-    def ShowlistStudents():
-        print("Show student list:")
-        for s in Student:
-            s.describe()
+def ShowlistStudents():
+    print("Show student List:")
+    for s in students:
+        s.describe()
 
-    @staticmethod
-    def ShowMarks():
-        courseName = input("Enter the name of course to show mark: ")
-        print("Marks " + courseName)
-        for StudentMark in StudentMarks:
-            if courseName == StudentMark.getCourse().getName():
-                StudentMark.describe()
 
-    @staticmethod
-    def Show_Average():
-        id = input("Enter student's id: ")
-        for s in Student:
-            if id == s.getId():
-                print("Name: " + s.getName() + " Avg: " + str(s.getAvg()))
+def ShowMarks():
+    courseName = input("Enter name of course to show marks: ")
+    print("Student Marks for " + courseName)
+    for studentMark in studentMarks:
+        if courseName == studentMark.getCourse().getName():
+            studentMark.describe()
