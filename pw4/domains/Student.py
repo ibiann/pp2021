f47ed269bbe6e4dde5ca64ce530b07b7b077d794
ibiann <trungpham152000@gmail.com>
2021-04-26 16:24:49 +0700

    fix pw4

---
+++ pw4/domains/Student.py
@@ -0,0 +1,22 @@
+
+
+class Student:
+    def __init__(self, id, name, dob):
+        self.__id = id
+        self.__name = name
+        self.__dob = dob
+
+    def describe(self):
+        print(self.__id + " " + self.__name + " " + self.__dob)
+
+    def getId(self):
+        return self.__id
+
+    def getName(self):
+        return self.__name
+
+    def getAvg(self):
+        return self.__avg
+
+    def setAvg(self, avg):
+        self.__avg = avg
